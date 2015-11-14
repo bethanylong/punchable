2823efc2eb650571da923af34e5701be6f978cef
Bethany Long <Bethany.Long@wwu.edu>
2015-11-14 08:18:24 -0800

    Preserve browser object; reorganization.

--- timesheet.py
+++ timesheet.py
@@ -14,9 +14,6 @@ from datetime import datetime
 class TimesheetError(Exception):
     pass
 
-def at_cas_login(br):
-    return br.url.startswith("https://websso.wwu.edu/cas/login")
-
 def auth():
     try:
         username = raw_input('Universal username: ')
@@ -25,6 +22,85 @@ def auth():
     except KeyboardInterrupt:
         print
         sys.exit(0)
+
+def fresh_browser():
+    return RoboBrowser(history=True, parser='html.parser')
+
+def at_cas_login(br):
+    return br.url.startswith("https://websso.wwu.edu/cas/login")
+
+def go_to_timesheet_home(br=None):
+    """Create a new browser object, log in via CAS, and return the object once
+       it's at the job selection page."""
+    if br is None:
+        br = fresh_browser()
+
+    # Log in via CAS
+    timesheet_url = 'https://admin.wwu.edu/pls/wwis/bwpktais.P_SelectTimeSheetRoll'
+    br.open(timesheet_url)
+
+
+    if at_cas_login(br):
+        cas_form = br.get_form(0)
+        cas_form['username'] = username
+        cas_form['password'] = password
+        br.submit_form(cas_form)
+
+    if at_cas_login(br):
+        raise TimesheetError("Username/password combination is incorrect, account is locked out, or something funky is happening with redirects.")
+
+    return br
+
+def get_timesheet_choice(br=None):
+    """Go to timesheet home page, gather list of timesheets/jobs, prompt user
+       for input on which one to choose, and return the choice's index and code."""
+    print
+    print 'Loading timesheet choices...'
+
+    br = go_to_timesheet_home(br)
+
+    # Look at timesheet selection form
+    jobs_form = br.get_forms()[-1]
+    job_fields = jobs_form.fields.getlist('Jobs')
+
+    # Get user's timesheet choice
+    job_options = [entry.options[0] for entry in job_fields]
+    poorly_scoped_table_classes = br.select('td.dedefault')
+    job_choice = get_job_option(poorly_scoped_table_classes, job_options) # prompt
+
+    #job_code = job_options[job_choice]
+    #return (job_choice, job_code)
+
+    jobs = []
+    if job_choice == len(job_options):
+        # "All of the above"
+        for index, job_code in enumerate(job_options):
+            jobs.append((index, job_code))
+    else:
+        jobs.append((job_choice, job_options[job_choice]))
+
+    return jobs
+
+def get_job_option(displayed_table_entries, job_options):
+    option_index = 0
+    for entry in displayed_table_entries:
+        try:
+            print '[{}] {} | {}'.format(option_index, prettify_job(entry.text), job_options[option_index])
+            option_index += 1
+        except:
+            pass
+    print '[{}] All of the above'.format(option_index)
+
+    job_choice = -1
+    while job_choice not in range(len(job_options) + 1):
+        try:
+            job_choice = int(raw_input('Select a job from above (index in square brackets): '))
+        except KeyboardInterrupt:
+            print
+            sys.exit(0)
+        except:
+            pass
+    return job_choice
 
 # job_text == br.select('td.dedefault')[0].text == 'Computer Assistant 5, S99554-00UR - Business Operations, 8050'
 def prettify_job(job_text):
@@ -52,35 +128,6 @@ def prepare_screwed_up_form(br, form, submit=None):
     serialized = payload.to_requests(method)
     return (method, url, serialized)
 
-def submit_screwed_up_form(br, method, url, serialized_form, **kwargs):
-    """Modified (part of) RoboBrowser.submit_form().
-       Accepts method, url, and the tweaked form to send.."""
-    send_args = br._build_send_args(**kwargs)
-    send_args.update(serialized_form)
-    response = br.session.request(method, url, **send_args)
-    br._update_state(response)
-
-def get_job_option(displayed_table_entries, job_options):
-    option_index = 0
-    for entry in displayed_table_entries:
-        try:
-            print '[{}] {} | {}'.format(option_index, prettify_job(entry.text), job_options[option_index])
-            option_index += 1
-        except:
-            pass
-    print '[{}] All of the above'.format(option_index)
-
-    job_choice = -1
-    while job_choice not in range(len(job_options) + 1):
-        try:
-            job_choice = int(raw_input('Select a job from above (index in square brackets): '))
-        except KeyboardInterrupt:
-            print
-            sys.exit(0)
-        except:
-            pass
-    return job_choice
-
 def timesheet_selection_post_data(serialized_form, job_choice, job_code):
     post_data_to_send = []
     job_count = 0
@@ -99,39 +146,13 @@ def timesheet_selection_post_data(serialized_form, job_choice, job_code):
         post_data_to_send.append((key, value))
     return post_data_to_send
 
-def matching_date(days, table_index):
-    """Iterate through list of seen days and return the index of a match if any.
-       Match based on index and page from timesheet <table>."""
-    for list_index, day in enumerate(days):
-        if day['index'] == table_index:
-            return list_index
-    # No match returned
-    raise ValueError("Unable to match this cell in the timesheet table with a date we've seen")
-
-def hour_value(table_cell):
-    if table_cell == 'Enter Hours':
-        return float(0)
-    else:
-        return float(table_cell)
-
-def parse_date(date_str):
-    return datetime.strptime(date_str, "%m/%d/%Y").date()
-
-def week_number(date):
-    # %W has Monday as the first day of the week, just like pay weeks.
-    return int(date.strftime("%W"))
-
-def weekday_number(date):
-    # %w has 0 as Sunday and 6 as Saturday, but we want 0 to be Monday
-    # and 6 to be Sunday.
-    canonical_weekday = int(date.strftime("%w"))
-    our_weekday = canonical_weekday - 1
-    if our_weekday == -1:
-        our_weekday = 6
-    return our_weekday
-
-def weekday_name(date):
-    return date.strftime("%a")
+def submit_screwed_up_form(br, method, url, serialized_form, **kwargs):
+    """Modified (part of) RoboBrowser.submit_form().
+       Accepts method, url, and the tweaked form to send.."""
+    send_args = br._build_send_args(**kwargs)
+    send_args.update(serialized_form)
+    response = br.session.request(method, url, **send_args)
+    br._update_state(response)
 
 def get_days_and_hours(br):
     """Return a list of objects including date and number of hours for each day
@@ -173,6 +194,40 @@ def get_days_and_hours(br):
              'hours': day['hours']}
             for day in days]
 
+def matching_date(days, table_index):
+    """Iterate through list of seen days and return the index of a match if any.
+       Match based on index and page from timesheet <table>."""
+    for list_index, day in enumerate(days):
+        if day['index'] == table_index:
+            return list_index
+    # No match returned
+    raise ValueError("Unable to match this cell in the timesheet table with a date we've seen")
+
+def hour_value(table_cell):
+    if table_cell == 'Enter Hours':
+        return float(0)
+    else:
+        return float(table_cell)
+
+def parse_date(date_str):
+    return datetime.strptime(date_str, "%m/%d/%Y").date()
+
+def week_number(date):
+    # %W has Monday as the first day of the week, just like pay weeks.
+    return int(date.strftime("%W"))
+
+def weekday_number(date):
+    # %w has 0 as Sunday and 6 as Saturday, but we want 0 to be Monday
+    # and 6 to be Sunday.
+    canonical_weekday = int(date.strftime("%w"))
+    our_weekday = canonical_weekday - 1
+    if our_weekday == -1:
+        our_weekday = 6
+    return our_weekday
+
+def weekday_name(date):
+    return date.strftime("%a")
+
 def hours_by_week(days_list):
     by_week = {}
     for day in days_list:
@@ -191,58 +246,10 @@ def print_hours(days_list):
             print 'Pay week {}, starting from {}: {} hours'.format(day['week'], day['date'], hbw[day['week']])
         print '{} {}: {} hours'.format(day['weekday_name'], day['date'], day['hours'])
 
-def go_to_timesheet_home():
-    """Create a new browser object, log in via CAS, and return the object once
-       it's at the job selection page."""
-    br = RoboBrowser(history=True, parser='html.parser')
-
-    # Log in via CAS
-    timesheet_url = 'https://admin.wwu.edu/pls/wwis/bwpktais.P_SelectTimeSheetRoll'
-    br.open(timesheet_url)
-    cas_form = br.get_form(0)
-    cas_form['username'] = username
-    cas_form['password'] = password
-    br.submit_form(cas_form)
-
-    if at_cas_login(br):
-        raise TimesheetError("Username/password combination is incorrect, account is locked out, or something funky is happening with redirects.")
-
-    return br
-
-def get_timesheet_choice():
-    """Go to timesheet home page, gather list of timesheets/jobs, prompt user
-       for input on which one to choose, and return the choice's index and code."""
-    print
-    print 'Loading timesheet choices...'
-
-    br = go_to_timesheet_home()
-
-    # Look at timesheet selection form
-    jobs_form = br.get_forms()[-1]
-    job_fields = jobs_form.fields.getlist('Jobs')
-
-    # Get user's timesheet choice
-    job_options = [entry.options[0] for entry in job_fields]
-    poorly_scoped_table_classes = br.select('td.dedefault')
-    job_choice = get_job_option(poorly_scoped_table_classes, job_options) # prompt
-
-    #job_code = job_options[job_choice]
-    #return (job_choice, job_code)
-
-    jobs = []
-    if job_choice == len(job_options):
-        # "All of the above"
-        for index, job_code in enumerate(job_options):
-            jobs.append((index, job_code))
-    else:
-        jobs.append((job_choice, job_options[job_choice]))
-
-    return jobs
-
-def print_summary(job_choice, job_code):
+def print_summary(job_choice, job_code, br=None):
     print
     print 'Getting hours for job {}...'.format(job_choice)
-    br = go_to_timesheet_home()
+    br = go_to_timesheet_home(br)
     jobs_form = br.get_forms()[-1]
 
     # Manipulate timesheet form with user's job choice, and submit it
@@ -287,5 +294,6 @@ def print_summary(job_choice, job_code):
 
 if __name__ == '__main__':
     username, password = auth()
-    for job_choice, job_code in get_timesheet_choice():
-        print_summary(job_choice, job_code)
+    br = fresh_browser()
+    for job_choice, job_code in get_timesheet_choice(br):
+        print_summary(job_choice, job_code, br)
